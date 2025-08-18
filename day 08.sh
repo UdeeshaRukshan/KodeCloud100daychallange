1066b4bd6254f0da2dcf178254556b03fe386cd2
Udeesha rukshan <76590372+UdeeshaRukshan@users.noreply.github.com>
2025-08-18 23:11:24 +0530

    Create day 08.sh

---
+++ day 08.sh
@@ -0,0 +1,9 @@
+which - is for identify the directory
+
+sudo yum install -y python3-pip   # (if CentOS/RHEL-based)
+# or
+sudo apt-get install -y python3-pip   # (if Ubuntu/Debian-based)
+
+sudo pip3 install ansible==4.7.0
+
+ansible --version
