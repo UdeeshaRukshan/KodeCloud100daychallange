61ae3030d42ae726f36b718da4bd2d40a054ac81
Udeesha rukshan <76590372+UdeeshaRukshan@users.noreply.github.com>
2025-09-02 19:38:41 +0000

    day 19 is now completed

---
+++ 100 Days of devops/day 19.sh
@@ -0,0 +1,11 @@
+install httpd server 
+
+change the configurations to correct port 
+
+and move the file throuht remote ssh
+
+move files to the /tmp/www/html/here 
+
+also the index.html files then restart the httpd server with 
+
+sudo chown apache:apache /tmp/www/html/here/index.html
